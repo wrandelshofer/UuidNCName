a675410234a6e4d16db29a3e41d3bd9f35459179
Werner Randelshofer <werner.randelshofer@fibermail.ch>
2023-02-05 20:25:47 +0100

    Implement UuidNcName from/to String converters.

---
+++ src/main/java/ch/randelshofer/uuidncname/UuidNCName.java
@@ -0,0 +1,279 @@
+package ch.randelshofer.uuidncname;
+
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.VarHandle;
+import java.math.BigInteger;
+import java.nio.ByteOrder;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+
+public class UuidNCName {
+    private final static VarHandle readLongBE =
+            MethodHandles.byteArrayViewVarHandle(long[].class, ByteOrder.BIG_ENDIAN);
+    private static final byte[] BASE_32_UPPER_CASE = {
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            '2', '3', '4', '5', '6', '7'
+    };
+    private static final byte[] BASE_64_URL_SAFE = {
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            '-', '_'
+    };
+    private static final byte[] BASE_32_LOWER_CASE = {
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            '2', '3', '4', '5', '6', '7'
+    };
+    private static final byte OTHER_CLASS = -1;
+    private static final byte[] CHAR_TO_BASE_64_MAP = new byte[128];
+    private static final byte[] CHAR_TO_BASE_32_MAP = new byte[128];
+    private final static BigInteger FIFTY_EIGHT = BigInteger.valueOf(58);
+
+    static {
+        for (char ch = 0; ch < CHAR_TO_BASE_64_MAP.length; ch++) {
+            CHAR_TO_BASE_64_MAP[ch] = OTHER_CLASS;
+        }
+        for (char ch = '0'; ch <= '9'; ch++) {
+            CHAR_TO_BASE_64_MAP[ch] = (byte) (ch - '0' + 52);
+        }
+        for (char ch = 'A'; ch <= 'Z'; ch++) {
+            CHAR_TO_BASE_64_MAP[ch] = (byte) (ch - 'A');
+        }
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            CHAR_TO_BASE_64_MAP[ch] = (byte) (ch - 'a' + 26);
+        }
+        CHAR_TO_BASE_64_MAP['-'] = 62;
+        CHAR_TO_BASE_64_MAP['_'] = 63;
+    }
+
+    static {
+        for (char ch = 0; ch < CHAR_TO_BASE_32_MAP.length; ch++) {
+            CHAR_TO_BASE_32_MAP[ch] = OTHER_CLASS;
+        }
+        for (char ch = '2'; ch <= '7'; ch++) {
+            CHAR_TO_BASE_32_MAP[ch] = (byte) (ch - '2' + 26);
+        }
+        for (char ch = 'A'; ch <= 'Z'; ch++) {
+            CHAR_TO_BASE_32_MAP[ch] = (byte) (ch - 'A');
+        }
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            CHAR_TO_BASE_32_MAP[ch] = (byte) (ch - 'a');
+        }
+    }
+
+    /**
+     * Don't let anyone instantiate this class.
+     */
+    public UuidNCName() {
+    }
+
+    public static String toBase32(UUID uuid) {
+        byte[] str = new byte[26];
+        str[0] = BASE_32_LOWER_CASE[uuid.version()];
+        str[25] = BASE_32_LOWER_CASE[compressVariant(uuid)];
+        encodeBase32(str, 1, compressMsb(uuid));
+        encodeBase32(str, 13, compressLsb(uuid));
+        return new String(str, StandardCharsets.ISO_8859_1);
+    }
+
+    private static void encodeBase32(byte[] str, int offset, long bits) {
+        for (int i = 11; i >= 0; i--) {
+            str[offset + i] = BASE_32_LOWER_CASE[(int) bits & 31];
+            bits >>>= 5;
+        }
+    }
+
+    private static long decodeBase32(byte[] str, int offset) {
+        long bits = 0;
+        for (int i = 0; i < 12; i++) {
+            byte ch = str[offset + i];
+            int value = ch < 0 ? OTHER_CLASS : CHAR_TO_BASE_32_MAP[ch];
+            if (value < 0) throw new IllegalArgumentException("Illegal character " + (char) ch);
+            bits = (bits << 5) | value;
+        }
+        return bits;
+    }
+
+    private static long decodeBase64(byte[] str, int offset) {
+        long bits = 0;
+        for (int i = 0; i < 10; i++) {
+            byte ch = str[offset + i];
+            int value = ch < 0 ? OTHER_CLASS : CHAR_TO_BASE_64_MAP[ch];
+            if (value < 0) throw new IllegalArgumentException("Illegal character " + (char) ch);
+            bits = (bits << 6) | value;
+        }
+        return bits;
+    }
+
+    private static void encodeBase64(byte[] str, int offset, long bits) {
+        for (int i = 9; i >= 0; i--) {
+            str[offset + i] = BASE_64_URL_SAFE[(int) bits & 63];
+            bits >>>= 6;
+        }
+    }
+
+    public static String toBase58(UUID uuid) {
+        byte[] b = new byte[23];
+        b[0] = BASE_32_UPPER_CASE[uuid.version()];
+        b[22] = BASE_32_UPPER_CASE[compressVariant(uuid)];
+
+        byte[] raw = new byte[15];
+        long msb = compressMsb(uuid);
+        readLongBE.set(raw, 0, msb << 4);
+        readLongBE.set(raw, 7, msb << 60 | compressLsb(uuid));
+        byte[] encoded = Base58.encode(raw).getBytes(StandardCharsets.ISO_8859_1);
+        System.arraycopy(encoded, 0, b, 1, encoded.length);
+        for (int i = encoded.length; i < 21; i++) {
+            b[i + 1] = '_';
+        }
+
+        return new String(b, StandardCharsets.ISO_8859_1);
+    }
+
+    public static String toBase58Lexical(UUID uuid) {
+        byte[] b = new byte[23];
+        b[0] = BASE_32_UPPER_CASE[uuid.version()];
+        b[22] = BASE_32_UPPER_CASE[compressVariant(uuid)];
+        byte[] raw = new byte[15];
+        long msb = compressMsb(uuid);
+        readLongBE.set(raw, 0, msb << 4);
+        readLongBE.set(raw, 7, msb << 60 | compressLsb(uuid));
+        byte[] encoded = Base58.encode(raw).getBytes(StandardCharsets.ISO_8859_1);
+        System.arraycopy(encoded, 0, b, 22 - encoded.length, encoded.length);
+        for (int i = 1; i < 22 - encoded.length; i++) {
+            b[i] = '1';
+        }
+        return new String(b, StandardCharsets.ISO_8859_1);
+    }
+
+    public static String toBase64(UUID uuid) {
+        byte[] str = new byte[22];
+        str[0] = BASE_32_UPPER_CASE[uuid.version()];
+        str[21] = BASE_32_UPPER_CASE[compressVariant(uuid)];
+        encodeBase64(str, 1, compressMsb(uuid));
+        encodeBase64(str, 11, compressLsb(uuid));
+        return new String(str, StandardCharsets.ISO_8859_1);
+    }
+
+    private static long compressMsb(UUID uuid) {
+        return Long.compress(uuid.getMostSignificantBits(), 0xffffffff_ffff_0fffL);
+    }
+
+    private static long compressLsb(UUID uuid) {
+        return Long.compress(uuid.getLeastSignificantBits(), 0x0fff_ffffffffffffL);
+    }
+
+    private static long decompressMsb(long bits, int version) {
+        return ((long) version << 12) | Long.expand(bits, 0xffffffff_ffff_0fffL);
+    }
+
+    private static long decompressLsb(long bits, int variant) {
+        return ((long) variant << 60) | bits;
+    }
+
+    private static long decompressMsb(byte[] bytes, int version) {
+        long bits = ((long) readLongBE.get(bytes, bytes.length - 16) << 4)
+                | ((long) readLongBE.get(bytes, bytes.length - 8) >>> 60);
+        return decompressMsb(bits, version);
+    }
+
+    private static long decompressLsb(byte[] bytes, int variant) {
+        long bits = (long) readLongBE.get(bytes, bytes.length - 8) & 0x0fff_ffffffffffffL;
+        return decompressLsb(bits, variant);
+    }
+
+    private static int compressVariant(UUID uuid) {
+        return (int) (uuid.getLeastSignificantBits() >>> 60);
+    }
+
+    public static UUID fromString(String string) {
+        return switch (string.length()) {
+            case 26 -> fromBase32(string);
+            case 23 -> fromBase58LexicalOrNonLexical(string);
+            case 22 -> fromBase64(string);
+            default -> UUID.fromString(string);
+        };
+    }
+
+    public static UUID fromBase32(String string) {
+        byte[] str = string.getBytes(StandardCharsets.ISO_8859_1);
+        if (str.length != 26)
+            throw new IllegalArgumentException("UUID string is " + string.length() + " characters long instead of 26.");
+        long msb = decodeBase32(str, 1);
+        long lsb = decodeBase32(str, 13);
+        int version = decodeVersion(str);
+        int variant = decodeVariant(str);
+        return new UUID(decompressMsb(msb, version), decompressLsb(lsb, variant));
+    }
+
+    public static UUID fromBase58(String string) {
+        byte[] str = string.getBytes(StandardCharsets.ISO_8859_1);
+        if (str.length != 23)
+            throw new IllegalArgumentException("UUID string is " + string.length() + " characters long instead of 23.");
+        int version = decodeVersion(str);
+        int variant = decodeVariant(str);
+        int endIndex = 22;
+        while (str[endIndex - 1] == '_' && endIndex > 1) {
+            endIndex--;
+        }
+        byte[] bytes = decodeBase58(string, endIndex);
+        long msb = decompressMsb(bytes, version);
+        long lsb = decompressLsb(bytes, variant);
+        return new UUID(msb, lsb);
+    }
+
+    private static byte[] decodeBase58(String string, int endIndex) {
+        byte[] bytes = Base58.decode(string.substring(1, endIndex));
+        if (bytes.length < 16) {
+            byte[] tmp = bytes;
+            bytes = new byte[16];
+            System.arraycopy(tmp, 0, bytes, bytes.length - tmp.length, tmp.length);
+        }
+        return bytes;
+    }
+
+    public static UUID fromBase58LexicalOrNonLexical(String string) {
+        byte[] str = string.getBytes(StandardCharsets.ISO_8859_1);
+        if (str.length != 23)
+            throw new IllegalArgumentException("UUID string is " + string.length() + " characters long instead of 23.");
+        if (str[21] == '_') return fromBase58(string);
+        else return fromBase58Lexical(string);
+    }
+
+    public static UUID fromBase58Lexical(String string) {
+        byte[] str = string.getBytes(StandardCharsets.ISO_8859_1);
+        if (str.length != 23)
+            throw new IllegalArgumentException("UUID string is " + string.length() + " characters long instead of 23.");
+        int version = decodeVersion(str);
+        int variant = decodeVariant(str);
+        byte[] bytes = decodeBase58(string, 22);
+        long msb = decompressMsb(bytes, version);
+        long lsb = decompressLsb(bytes, variant);
+        return new UUID(msb, lsb);
+    }
+
+    private static int decodeVersion(byte[] str) {
+        byte ch = str[0];
+        int version = ch < 0 ? OTHER_CLASS : CHAR_TO_BASE_32_MAP[ch];
+        if (version < 0) throw new IllegalArgumentException("Illegal version character: " + (char) ch);
+        return version;
+    }
+
+    private static int decodeVariant(byte[] str) {
+        byte ch = str[str.length - 1];
+        int variant = ch < 0 ? OTHER_CLASS : CHAR_TO_BASE_32_MAP[ch];
+        if (variant < 0) throw new IllegalArgumentException("Illegal version character: " + (char) ch);
+        return variant;
+    }
+
+    public static UUID fromBase64(String string) {
+        byte[] str = string.getBytes(StandardCharsets.ISO_8859_1);
+        if (str.length != 22)
+            throw new IllegalArgumentException("UUID string is " + string.length() + " characters long instead of 22.");
+        long msb = decodeBase64(str, 1);
+        long lsb = decodeBase64(str, 11);
+        int version = decodeVersion(str);
+        int variant = decodeVariant(str);
+        return new UUID(decompressMsb(msb, version), decompressLsb(lsb, variant));
+    }
+}
---
+++ src/test/java/ch/randelshofer/uuidncname/UuidNCNameTest.java
@@ -0,0 +1,107 @@
+package ch.randelshofer.uuidncname;
+
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+
+class UuidNCNameTest {
+    @TestFactory
+    public List<DynamicTest> dynamicTests_base32() {
+        return List.of(
+                dynamicTest("00000000-0000-0000-0000-000000000000", () -> testBase32("00000000-0000-0000-0000-000000000000", "aaaaaaaaaaaaaaaaaaaaaaaaaa")),
+                dynamicTest("ca6be4c8-cbaf-11ea-b2ab-00045a86c8a1", () -> testBase32("ca6be4c8-cbaf-11ea-b2ab-00045a86c8a1", "bzjv6jsglv4pkfkyaarninsfbl")),
+                dynamicTest("000003e8-cbb9-21ea-b201-00045a86c8a1", () -> testBase32("000003e8-cbb9-21ea-b201-00045a86c8a1", "caaaah2glxepkeaiaarninsfbl")),
+                dynamicTest("3d813cbb-47fb-32ba-91df-831e1593ac29", () -> testBase32("3d813cbb-47fb-32ba-91df-831e1593ac29", "dhwatzo2h7mv2dx4ddykzhlbjj")),
+                dynamicTest("01867b2c-a0dd-459c-98d7-89e545538d6c", () -> testBase32("01867b2c-a0dd-459c-98d7-89e545538d6c", "eagdhwlfa3vm4rv4j4vcvhdlmj")),
+                dynamicTest("21f7f8de-8051-5b89-8680-0195ef798b6a", () -> testBase32("21f7f8de-8051-5b89-8680-0195ef798b6a", "feh37rxuakg4jnaabsxxxtc3ki"))
+        );
+    }
+    @TestFactory
+    public List<DynamicTest> dynamicTests_base58() {
+        return List.of(
+                dynamicTest("00000000-0000-0000-0000-000000000000", () -> testBase58("00000000-0000-0000-0000-000000000000", "A111111111111111______A")),
+                dynamicTest("ca6be4c8-cbaf-11ea-b2ab-00045a86c8a1", () -> testBase58("ca6be4c8-cbaf-11ea-b2ab-00045a86c8a1", "B6fTkmTD22KpWbDq1LuiszL")),
+                dynamicTest("000003e8-cbb9-21ea-b201-00045a86c8a1", () -> testBase58("000003e8-cbb9-21ea-b201-00045a86c8a1", "C11KtP6Y9P3rRkvh2N1e__L")),
+                dynamicTest("3d813cbb-47fb-32ba-91df-831e1593ac29", () -> testBase58("3d813cbb-47fb-32ba-91df-831e1593ac29", "D2ioV6oTr9yq6dMojd469nJ")),
+                dynamicTest("01867b2c-a0dd-459c-98d7-89e545538d6c", () -> testBase58("01867b2c-a0dd-459c-98d7-89e545538d6c", "E3UZ99RxxUJC1v4dWsYtb_J")),
+                dynamicTest("21f7f8de-8051-5b89-8680-0195ef798b6a", () -> testBase58("21f7f8de-8051-5b89-8680-0195ef798b6a", "Fx7wEJfz9eb1TYzsrT7Zs_I"))
+        );
+    }
+    @TestFactory
+    public List<DynamicTest> dynamicTests_base58_lexical() {
+        return List.of(
+                dynamicTest("00000000-0000-0000-0000-000000000000", () -> testBase58Lexical("00000000-0000-0000-0000-000000000000", "A111111111111111111111A")),
+                dynamicTest("ca6be4c8-cbaf-11ea-b2ab-00045a86c8a1", () -> testBase58Lexical("ca6be4c8-cbaf-11ea-b2ab-00045a86c8a1", "B6fTkmTD22KpWbDq1LuiszL")),
+                dynamicTest("000003e8-cbb9-21ea-b201-00045a86c8a1", () -> testBase58Lexical("000003e8-cbb9-21ea-b201-00045a86c8a1", "C1111KtP6Y9P3rRkvh2N1eL")),
+                dynamicTest("3d813cbb-47fb-32ba-91df-831e1593ac29", () -> testBase58Lexical("3d813cbb-47fb-32ba-91df-831e1593ac29", "D2ioV6oTr9yq6dMojd469nJ")),
+                dynamicTest("01867b2c-a0dd-459c-98d7-89e545538d6c", () -> testBase58Lexical("01867b2c-a0dd-459c-98d7-89e545538d6c", "E13UZ99RxxUJC1v4dWsYtbJ")),
+                dynamicTest("21f7f8de-8051-5b89-8680-0195ef798b6a", () -> testBase58Lexical("21f7f8de-8051-5b89-8680-0195ef798b6a", "F1x7wEJfz9eb1TYzsrT7ZsI"))
+        );
+    }
+    @TestFactory
+    public List<DynamicTest> dynamicTests_base64() {
+        return List.of(
+                dynamicTest("00000000-0000-0000-0000-000000000000", () -> testBase64("00000000-0000-0000-0000-000000000000", "AAAAAAAAAAAAAAAAAAAAAA")),
+                dynamicTest("ca6be4c8-cbaf-11ea-b2ab-00045a86c8a1", () -> testBase64("ca6be4c8-cbaf-11ea-b2ab-00045a86c8a1", "BymvkyMuvHqKrAARahsihL")),
+                dynamicTest("000003e8-cbb9-21ea-b201-00045a86c8a1", () -> testBase64("000003e8-cbb9-21ea-b201-00045a86c8a1", "CAAAD6Mu5HqIBAARahsihL")),
+                dynamicTest("3d813cbb-47fb-32ba-91df-831e1593ac29", () -> testBase64("3d813cbb-47fb-32ba-91df-831e1593ac29", "DPYE8u0f7K6Hfgx4Vk6wpJ")),
+                dynamicTest("01867b2c-a0dd-459c-98d7-89e545538d6c", () -> testBase64("01867b2c-a0dd-459c-98d7-89e545538d6c", "EAYZ7LKDdWcjXieVFU41sJ")),
+                dynamicTest("21f7f8de-8051-5b89-8680-0195ef798b6a", () -> testBase64("21f7f8de-8051-5b89-8680-0195ef798b6a", "FIff43oBRuJaAAZXveYtqI"))
+        );
+    }
+
+
+
+
+
+
+    private void testBase32(String canonicalString, String expectedString) {
+        UUID expectedUuid = UUID.fromString(canonicalString);
+        String actualString = UuidNCName.toBase32(expectedUuid);
+        UUID actualUuid =    UuidNCName.fromBase32(expectedString);
+        UUID actualUuid2 =    UuidNCName.fromString(expectedString);
+        UUID actualUuid3 =    UuidNCName.fromString(canonicalString);
+        assertEquals(expectedString,actualString);
+        assertEquals(expectedUuid,actualUuid);
+        assertEquals(expectedUuid,actualUuid2);
+        assertEquals(expectedUuid,actualUuid3);
+    }
+    private void testBase58(String canonicalString, String expectedString) {
+        UUID expectedUuid = UUID.fromString(canonicalString);
+        String actual = UuidNCName.toBase58(expectedUuid);
+        UUID actualUuid =   UuidNCName.fromBase58(expectedString);
+        UUID actualUuid2 =    UuidNCName.fromString(expectedString);
+        UUID actualUuid3 =    UuidNCName.fromString(canonicalString);
+        assertEquals(expectedString,actual);
+        assertEquals(expectedUuid,actualUuid);
+        assertEquals(expectedUuid,actualUuid2);
+        assertEquals(expectedUuid,actualUuid3);
+    }
+    private void testBase58Lexical(String canonicalString, String expectedString) {
+        UUID expectedUuid = UUID.fromString(canonicalString);
+        String actual = UuidNCName.toBase58Lexical(expectedUuid);
+        UUID actualUuid =     UuidNCName.fromBase58Lexical(expectedString);
+        UUID actualUuid2 =    UuidNCName.fromString(expectedString);
+        UUID actualUuid3 =    UuidNCName.fromString(canonicalString);
+        assertEquals(expectedString,actual);
+        assertEquals(expectedUuid,actualUuid);
+        assertEquals(expectedUuid,actualUuid2);
+        assertEquals(expectedUuid,actualUuid3);
+    }
+    private void testBase64(String canonicalString, String expectedString) {
+        UUID expectedUuid = UUID.fromString(canonicalString);
+        String actual = UuidNCName.toBase64(expectedUuid);
+        UUID actualUuid =    UuidNCName.fromBase64(expectedString);
+        UUID actualUuid2 =    UuidNCName.fromString(expectedString);
+        UUID actualUuid3 =    UuidNCName.fromString(canonicalString);
+        assertEquals(expectedString,actual);
+        assertEquals(expectedUuid,actualUuid);
+        assertEquals(expectedUuid,actualUuid2);
+        assertEquals(expectedUuid,actualUuid3);
+    }
+}
