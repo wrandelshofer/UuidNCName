7ef34218e05c05a941707c7e26092d1d254a18dd
Werner Randelshofer <werner.randelshofer@fibermail.ch>
2023-03-18 11:08:35 +0100

    Run JMH benchmarks.

--- src/test/java/ch/randelshofer/uuidncname/JmhUuidNcName.java
+++ src/test/java/ch/randelshofer/uuidncname/JmhUuidNcName.java
@@ -68,7 +68,7 @@ import java.util.concurrent.TimeUnit;
 @Fork(value = 1, jvmArgsAppend = {
         "-Xmx4g"
 })
-@Measurement(iterations = 2)
+@Measurement(iterations = 1)
 @Warmup(iterations = 2)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @BenchmarkMode(Mode.AverageTime)
@@ -85,62 +85,62 @@ public class JmhUuidNcName {
     private String base64 = UuidNCName.toString(uuid, UuidFormat.NCNAME_64);
     private String base64Lex = UuidNCName.toString(uuid, UuidFormat.NCNAME_64_LEX);
 
-    /*
-        @Benchmark
-        public UUID rBase32() {
-            return UuidNCName.fromString(base32);
-        }
 
-        @Benchmark
-        public UUID rBase32Lex() {
-            return UuidNCName.fromString(base32Lex);
-        }
+    @Benchmark
+    public UUID rBase32() {
+        return UuidNCName.fromString(base32);
+    }
 
-        @Benchmark
-        public UUID rBase58() {
-            return UuidNCName.fromString(base58);
-        }
+    @Benchmark
+    public UUID rBase32Lex() {
+        return UuidNCName.fromString(base32Lex);
+    }
 
-        @Benchmark
-        public UUID rBase58Lex() {
-            return UuidNCName.fromString(base58Lex);
-        }
+    @Benchmark
+    public UUID rBase58() {
+        return UuidNCName.fromString(base58);
+    }
 
-        @Benchmark
-        public UUID rBase64() {
-            return UuidNCName.fromString(base64);
-        }
+    @Benchmark
+    public UUID rBase58Lex() {
+        return UuidNCName.fromString(base58Lex);
+    }
 
-        @Benchmark
-        public UUID rBase64Lex() {
-            return UuidNCName.fromString(base64Lex);
-        }
+    @Benchmark
+    public UUID rBase64() {
+        return UuidNCName.fromString(base64);
+    }
 
-        @Benchmark
-        public UUID rCanonical() {
-            return UUID.fromString(canonical);
-        }
+    @Benchmark
+    public UUID rBase64Lex() {
+        return UuidNCName.fromString(base64Lex);
+    }
 
-        @Benchmark
-        public String wBase32() {
-            return UuidNCName.toString(uuid, UuidFormat.NCNAME_32);
-        }
+    @Benchmark
+    public UUID rCanonical() {
+        return UUID.fromString(canonical);
+    }
 
-        @Benchmark
-        public String wBase32Lex() {
-            return UuidNCName.toString(uuid, UuidFormat.NCNAME_32_LEX);
-        }
+    @Benchmark
+    public String wBase32() {
+        return UuidNCName.toString(uuid, UuidFormat.NCNAME_32);
+    }
 
-        @Benchmark
-        public String wBase58() {
-            return UuidNCName.toString(uuid, UuidFormat.NCNAME_58);
-        }
-    */
+    @Benchmark
+    public String wBase32Lex() {
+        return UuidNCName.toString(uuid, UuidFormat.NCNAME_32_LEX);
+    }
+
+    @Benchmark
+    public String wBase58() {
+        return UuidNCName.toString(uuid, UuidFormat.NCNAME_58);
+    }
+
     @Benchmark
     public String wBase58Lex() {
         return UuidNCName.toString(uuid, UuidFormat.NCNAME_58_LEX);
     }
-/*
+
     @Benchmark
     public String wBase64() {
         return UuidNCName.toString(uuid, UuidFormat.NCNAME_64);
@@ -156,6 +156,4 @@ public class JmhUuidNcName {
     public String wCanonical() {
         return uuid.toString();
     }
-
- */
 }
